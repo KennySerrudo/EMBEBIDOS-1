f6e441aa900933bdf671a730f3c3dac559cedbfc
KennySerrudo <kennyserrudo8@gmail.com>
2024-08-12 19:27:51 -0400

    refresh codes

---
+++ Arduino Codes/blink/blink/blink.ino
@@ -0,0 +1,15 @@
+#define LED 13
+
+#define time 1500
+void setup() {
+  // put your setup code here, to run once:
+pinMode (LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(LED, !digitalWrite(LED));
+delay(time);
+
+
+}
